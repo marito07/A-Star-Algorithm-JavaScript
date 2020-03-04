e5f9076d935954a1f96252a94cb78dc516bb48c2
Mario Torres <48029021+marito07@users.noreply.github.com>
2020-03-04 17:36:25 +0100

    Lee los datos de la Tabla
    + Mejoras de Diseño de los botones
    + Añadidas limitaciones de los botones
    + Lógica de Leer Tabla

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-    let variable = false;
     $("#buttonRow").click(function () {
         let texto ="";
         $("#fila1").find("td").each(function(){
@@ -13,32 +12,133 @@ $(function () {
             $(this).append("<td></td>");
         })
     });
+
     $("#botonInicio").click( function(){
-		$(this).toggleClass("pressed");
+        $(this).toggleClass("pressed");
+        if($("#botonMeta").is(":disabled")){
+            $("#botonMeta").prop('disabled', false);
+        }
+        else{
+            $("#botonMeta").prop('disabled', true);
+        }
+        if($("#botonCasillasProhibidas").is(":disabled")){
+            $("#botonCasillasProhibidas").prop('disabled', false);
+        }
+        else{
+            $("#botonCasillasProhibidas").prop('disabled', true);
+        }
     } );
+
     $("#botonMeta").click( function(){
-		$(this).toggleClass("pressed");
+        $(this).toggleClass("pressed");
+        if($("#botonInicio").is(":disabled")){
+            $("#botonInicio").prop('disabled', false);
+        }
+        else{
+            $("#botonInicio").prop('disabled', true);
+        }
+        if($("#botonCasillasProhibidas").is(":disabled")){
+            $("#botonCasillasProhibidas").prop('disabled', false);
+        }
+        else{
+            $("#botonCasillasProhibidas").prop('disabled', true);
+        }
     } );
+    $("#botonCasillasProhibidas").click( function(){
+        $(this).toggleClass("pressed");
+        if($("#botonMeta").is(":disabled")){
+            $("#botonMeta").prop('disabled', false);
+        }
+        else{
+            $("#botonMeta").prop('disabled', true);
+        }
+        if($("#botonInicio").is(":disabled")){
+            $("#botonInicio").prop('disabled', false);
+        }
+        else{
+            $("#botonInicio").prop('disabled', true);
+        }
+    } );
+    
+
     var colorInicio = null;
     $(document).on("click", "td",function(){
         if( $("#botonInicio").hasClass("pressed") == true ){
             if(colorInicio != null){
-                $(colorInicio).removeClass("tdcolorao");
+                $(colorInicio).removeClass("tdInicio");
             }
 
             colorInicio = this;
-            $(colorInicio).toggleClass("tdcolorao");
+            $(colorInicio).toggleClass("tdInicio");
         }
     })
 
+    var colorInicio = null;
+    $(document).on("click", "td",function(){
+        if( $("#botonInicio").hasClass("pressed") == true ){
+            if(colorInicio != null){
+                $(colorInicio).removeClass("tdInicio");
+            }
+
+            colorInicio = this;
+            $(colorInicio).toggleClass("tdInicio");
+        }
+    })
+    
     var colorMeta = null;
     $(document).on("click", "td",function(){
         if( $("#botonMeta").hasClass("pressed") == true ){
             if(colorMeta != null){
-                $(colorMeta).removeClass("tdamari");
+                $(colorMeta).removeClass("tdMeta");
             }
             colorMeta = this;
-            $(colorMeta).toggleClass("tdamari");
+            $(colorMeta).toggleClass("tdMeta");
         }
     })
+
+    $("#resolverAlgoritmo").click( function(){
+
+        /* Deshabilitamos los botones */
+        $("#botonesAccion").find('button').each(function(){
+            $(this).prop('disabled', true);
+        })
+        let numFilas = 1;
+        let numColumnas = 1;
+		$("#tabla").find("tr").each(function(){
+            numFilas++;
+        });
+        $("#fila1").find("td").each(function(){
+            numColumnas++;
+        });
+
+        let tablita = [];
+        let i = 0;
+        let casillaInicio = {}, casillaMeta = {};
+        $("#tabla").find("tr").each(function(){
+            tablita[i] = [];
+            let j = 0;
+            $(this).find("td").each(function(){
+                if($(this).hasClass("tdInicio") == true ){
+                    tablita[i][j] = 1;
+                    casillaInicio.fila = i;
+                    casillaInicio.col = j; 
+                }
+                else if($(this).hasClass("tdMeta") == true ){
+                    tablita[i][j] = 3;
+                    casillaMeta.fila = i;
+                    casillaMeta.col = j; 
+                }
+                else{
+                    tablita[i][j] = 0;
+                }
+                j++;
+            });
+            i++;
+        });
+
+
+
+
+    } );
+    
 });
