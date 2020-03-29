16b68c655e845ba54eccf79e222ad59413d7426f
Mario Torres <48029021+marito07@users.noreply.github.com>
2020-03-29 19:57:05 +0200

    Funcionalidad completa y correcta

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,14 @@
 $(function () {
 
 
-    let numFilas = 5; // Numero de filas de la tabla
-    let numColumnas = 5; // Numero de columnas de la tabla
+    var numFilas = 5; // Numero de filas de la tabla
+    var numColumnas = 5; // Numero de columnas de la tabla
 
     // Funcion que añade una fila nueva a la tabla
     $("#buttonRow").click(function () {
-        let texto = "";
+        var texto = "";
         numFilas = numFilas + 1;
-        for(let i = 0; i < numColumnas; i++){
+        for(var i = 0; i < numColumnas; i++){
             texto += "<td></td>"
         }
         $("#tabla").append("<tr class=\"fila\"> <th scope=\"row\">" + numFilas + "</th>" + texto + "</tr>");
@@ -112,6 +112,18 @@ $(function () {
     })
 
 
+    function Nodo(x,y){
+        this.x = x;
+        this.y = y;
+
+        this.f = 0;
+        this.h = 0;
+        this.g = 0;
+
+        this.padre = null;
+        this.sucesores = [];
+    }
+
     $("#resolverAlgoritmo").click(function () {
         /* Deshabilitamos los botones */
         $("#botonesAccion").find('button').each(function () {
@@ -121,67 +133,150 @@ $(function () {
 
         var ABIERTA = [];
         var CERRADA = [];
-
+        var camino = [];
 
         // Leemos de la tabla la posición del INICIO, de la META y de las CASILLAS PROHIBIDAS
-        let tablita = [];
-        let i = 1;
-        let casillaInicio = {}, casillaMeta = {};
+        var tabla;
+        var i = 1;
+        var casillaInicio, casillaMeta;
+        tabla = new Array(numFilas);
         $("#tabla").find(".fila").each(function () {
-            tablita[i] = [];
-            let j = 1;
+            tabla[i-1] = new Array(numColumnas);
+            var j = 1;
             $(this).find("td").each(function () {
+                tabla[i-1][j-1] = new Nodo(i,j);
                 if ($(this).hasClass("tdInicio") == true) {
-                    //tablita[i][j] = 1;
-                    casillaInicio.fila = i;
-                    casillaInicio.col = j;
+                    casillaInicio = tabla[i-1][j-1];
                 }
                 else if ($(this).hasClass("tdMeta") == true) {
-                    //tablita[i][j] = 3;
-                    casillaMeta.fila = i;
-                    casillaMeta.col = j;
+                    casillaMeta = tabla[i-1][j-1];
                 }
                 else if ($(this).hasClass("prohibida") == true){
-                    CERRADA.push( // Los obstaculos se incluyen directamente en la lista cerrada
-                        {fila: i,
-                        col: j}
-                    )
+                    CERRADA.push(tabla[i-1][j-1]);
                 }
                 j++;
             });
             i++;
         });
 
-        /* console.log(tablita);
-        console.log(casillaInicio);
-        console.log(casillaMeta); */
-        console.log(CERRADA);
-
         //Aquí comienza el algoritmo como tal
-        let salida = false;
+        var salida = false;
         ABIERTA.push(casillaInicio);
-        /* if(salida != true){
-            if(ABIERTA.length > 0){
-                let posSeleccionado = 0;
-                let seleccionado; 
 
-                for(let i = 0; i < ABIERTA.length; i++){
+        while(salida != true){
+            if(ABIERTA.length > 0){
+                var posSeleccionado = 0;
+                var seleccionado = ABIERTA[0]; 
+
+                for(var i = 0; i < ABIERTA.length; i++){
+                    if(ABIERTA[i].f < ABIERTA[posSeleccionado].f){
+                        posSeleccionado = i;
+                        seleccionado = ABIERTA[i];
+                    }
+                }
+                console.log(`El Seleccionado es: `);
+                console.log(seleccionado);
+                console.log("El final es")
+                console.log(casillaMeta);
+                if(seleccionado === casillaMeta){ // Si se llega al final
+                    console.log("Llega al final");
+                    var aux = seleccionado;
+                    camino.push(aux);
+
+                    while(aux.padre !== null){
+                        aux = aux.padre;
+                        camino.push(aux);
+                    }
+
+                    salida = true;
+                }
+                else{ // Si no se llega al final
+                    // Borramos el elemento de la lista ABIERTA y lo añadimos a la CERRADA
+                    ABIERTA.splice(posSeleccionado,1);
+                    CERRADA.push(seleccionado);
+
+                    var sucesores = []; // Lista de Nodos sucesores del seleccionado;
+                    var ValoresG = [];
+                    if(seleccionado.x - 1 > 0){ // El de la izquierda
+                        sucesores.push(tabla[seleccionado.x - 2][seleccionado.y - 1]);
+                        ValoresG.push(1);
+                    }
+                    if(seleccionado.x < numFilas){ // El de la derecha
+                        sucesores.push(tabla[seleccionado.x][seleccionado.y - 1]);
+                        ValoresG.push(1);
+                    }
+                    if(seleccionado.y - 1 > 0){ // El de arriba
+                        sucesores.push(tabla[seleccionado.x - 1][seleccionado.y - 2]);
+                        ValoresG.push(1);
+                    }
+                    if(seleccionado.y < numColumnas){ // El de abajo
+                        sucesores.push(tabla[seleccionado.x - 1][seleccionado.y]);
+                        ValoresG.push(1);
+                    }
+                    if(seleccionado.y - 1 > 0 && seleccionado.x - 1 > 0){ // El de arriba a la izquierda
+                        sucesores.push(tabla[seleccionado.x - 2][seleccionado.y - 2]);
+                        ValoresG.push(Math.sqrt(2));
+                    }
+                    if(seleccionado.y - 1 > 0 && seleccionado.x < numFilas){ // El de arriba a la derecha
+                        sucesores.push(tabla[seleccionado.x][seleccionado.y - 2]);
+                        ValoresG.push(Math.sqrt(2));
+                    }
+                    if(seleccionado.y < numColumnas && seleccionado.x - 1 > 0){ // El de abajo a la izquierda
+                        sucesores.push(tabla[seleccionado.x - 2][seleccionado.y]);
+                        ValoresG.push(Math.sqrt(2));
+                    }
+                    if(seleccionado.y < numColumnas && seleccionado.x < numFilas){ // El de abajo a la derecha
+                        sucesores.push(tabla[seleccionado.x][seleccionado.y]);
+                        ValoresG.push(Math.sqrt(2));
+                    }
+
+                    for(var i = 0; i < sucesores.length; i++){
+                        var sucesor = sucesores[i];
+                        if(!CERRADA.includes(sucesor)){
+                            var newG = seleccionado.g + ValoresG[i];
+                            var newH = h(sucesor, casillaMeta);
+                            var newF = newG + newH;
+
+                            if(ABIERTA.includes(sucesor)){
+                                if(newF < sucesor.f){
+                                    sucesor.g = newG;
+                                    sucesor.h = newH;
+                                    sucesor.f = newF;
+                                    sucesor.padre = seleccionado;
+                                }
+                            }
+                            else{
+                                sucesor.g = newG;
+                                sucesor.padre = seleccionado;
+                                sucesor.h = newH;
+                                ABIERTA.push(sucesor);
+                            }
+
+
+                            // valores de g y f
+
+                            /* sucesor.h = h(sucesor, casillaMeta);
+                            console.log("---")
+                            console.log("Sucesor G:")
+                            console.log(sucesor.g);
+                            console.log("---")
+                            sucesor.f = sucesor.g + sucesor.h;
+                             */
+                            
+                        }
+                    }
 
                 }
             }
-        } */
+            else {
+                salida = true;
+            }
+        } 
+
+        console.log(camino);
 
     });
 
-    // Funcion f(n)
-    function f(casillaX, casillaMeta, casillaInicio){
-        
-    }
-
-    // Función g(n)
-    function g(casillaX, casillaMeta, casillaInicio){
-
-    }
 
     // Función h(n)
     function h(casillaX, casillaMeta){
